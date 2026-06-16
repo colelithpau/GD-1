225f6e05b78fd4c3c77636b6d03559b20aa81b54
blackgrid <jppaulo@student.apc.edu.ph>
2026-06-16 10:13:58 +0800

    Initialize maze and player in level13.py
    Spawn a maze
    Spawn a hero 
    Add a goal

---
+++ level13.py
@@ -0,0 +1,7 @@
+# Spawn a maze. Change the number for a different maze!
+game.spawnMaze("forest", 7)
+
+# Spawn a hero with spawnPlayerXY(type, x, y)
+game.spawnPlayerXY("captain", 29, 37)
+# Add at least one goal!
+game.addSurviveGoal(20)
