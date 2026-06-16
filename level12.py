074ca8e2b535aa0cb7811bbf87a0f5cae5582e30
blackgrid <jppaulo@student.apc.edu.ph>
2026-06-16 10:12:01 +0800

    Add archers and soldier spawning to level12

---
+++ level12.py
@@ -0,0 +1,11 @@
+# Archers are powerful ranged units!
+# While soldiers have health, archers provide damage.
+
+# Too many soldiers, not enough archers!
+game.spawnXY("soldier", 20, 40)
+
+
+game.spawnXY("archer", 10, 30)
+game.spawnXY("archer", 9, 18)
+game.spawnXY("archer", 9, 45)
+
